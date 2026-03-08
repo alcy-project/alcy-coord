792cbed3a9d3d3a6bfd89de4c053caebc8a146e0
pugur <pugurmc@gmail.com>
2026-03-08 19:32:18 +0900

    style: add license header

--- tests/add_test.cc
+++ tests/add_test.cc
@@ -1,4 +1,9 @@
+// Copyright 2026 pugur
+// This source code is licensed under the Apache License, Version 2.0 with LLVM
+// Exceptions which can be found in the LICENSE file.
+
 #define CATCH_CONFIG_MAIN
+
 #include "base/add.h"
 
 #include <catch2/catch_all.hpp>
