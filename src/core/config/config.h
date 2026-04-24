df49dc499643b5e63730b9ffafaa6717456be9b4
pugur <pugurmc@gmail.com>
2026-04-25 06:24:06 +0900

    feat: add core module

---
+++ src/core/config/config.h
@@ -0,0 +1,15 @@
+// Copyright 2026 pugur
+// This source code is licensed under the Apache License, Version 2.0 with LLVM
+// Exceptions which can be found in the LICENSE file.
+
+#pragma once
+
+#include "fpag/base/numeric.h"
+
+namespace core {
+
+struct Config {
+  u8 test = 0;
+};
+
+}  // namespace core
