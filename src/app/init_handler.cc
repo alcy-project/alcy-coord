7ad61fd8c21ac172b3461518b2dbc43fdcaae9c9
pugur <pugurmc@gmail.com>
2026-04-25 06:22:00 +0900

    feat: add init handler

---
+++ src/app/init_handler.cc
@@ -0,0 +1,19 @@
+// Copyright 2026 pugur
+// This source code is licensed under the Apache License, Version 2.0 with LLVM
+// Exceptions which can be found in the LICENSE file.
+
+#include "app/init_handler.h"
+
+#include "fpag/base/console.h"
+#include "fpag/base/debug/signal_handler.h"
+#include "fpag/base/debug/terminate_handler.h"
+
+namespace app {
+
+void init() {
+  base::register_console();
+  base::register_terminate_handler();
+  base::register_signal_handlers();
+}
+
+}  // namespace app
