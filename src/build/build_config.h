b17fd346acd2dbe5e8d4dbfa8893185ef7268c2b
pugur <pugurmc@gmail.com>
2026-04-25 06:22:44 +0900

    feat: add BUILD_FLAG macro

---
+++ src/build/build_config.h
@@ -0,0 +1,10 @@
+// Copyright 2026 pugur
+// This source code is licensed under the Apache License, Version 2.0 with LLVM
+// Exceptions which can be found in the LICENSE file.
+
+#pragma once
+
+#include "fpag/build/build_config.h"
+
+#define BUILD_FLAG(flag) FPAG_BUILD_FLAG(flag)
+
