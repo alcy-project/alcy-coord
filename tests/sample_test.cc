e5f0342937fc41d9ace3f6b184e8237534d6a6ab
pugur <pugurmc@gmail.com>
2026-04-26 02:10:05 +0900

    tests: temporarily disable LlvmIrEmitter test

---
+++ tests/sample_test.cc
@@ -0,0 +1,10 @@
+// Copyright 2026 pugur
+// This source code is licensed under the Apache License, Version 2.0 with LLVM
+// Exceptions which can be found in the LICENSE file.
+
+#include "catch2/catch_all.hpp"
+
+TEST_CASE("test", "[sample]") {
+  CHECK(1 + 1 == 2);
+}
+
