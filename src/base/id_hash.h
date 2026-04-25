e81c8f8cd4e10b1c0130fee1a191f7a62606f2b7
pugur <pugurmc@gmail.com>
2026-04-26 02:09:01 +0900

    fix: add missing header include guard

--- src/base/id_hash.h
+++ src/base/id_hash.h
@@ -2,6 +2,8 @@
 // This source code is licensed under the Apache License, Version 2.0 with LLVM
 // Exceptions which can be found in the LICENSE file.
 
+#pragma once
+
 #include <functional>
 
 #include "base/id.h"
